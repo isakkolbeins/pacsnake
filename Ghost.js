a7a30dd9c3a079709339001d782a5bb4b5c94019
Adam Jens <ajj10@hi.is>
2018-11-20 20:58:59 +0000

    ghost orðið flott

--- Ghost.js
+++ Ghost.js
@@ -20,27 +20,27 @@ function Ghost(descr) {
 
     this.randomisePosition();
 
-
-    if(!this.color){
+    if (!this.color) {
         this.color = 0;
     }
 
+    //different colors for the ghosts
     switch (this.color) {
         case 0:
-        this.sprite = g_sprites.ghostRed;
+            this.sprite = g_sprites.ghostRed;
             break;
         case 1:
-        this.sprite = g_sprites.ghostOrange;
+            this.sprite = g_sprites.ghostOrange;
             break;
         case 2:
-        this.sprite = g_sprites.ghostPink;
+            this.sprite = g_sprites.ghostPink;
             break;
         case 3:
-        this.sprite = g_sprites.ghostBlue;
+            this.sprite = g_sprites.ghostBlue;
             break;
 
         default:
-        this.sprite = this.sprite.ghostBlue;
+            this.sprite = this.sprite.ghostBlue;
             break;
     }
 
@@ -52,34 +52,35 @@ function Ghost(descr) {
 
     this.rememberResets();
 
-
 };
 
 Ghost.prototype = new Entity();
 
 Ghost.prototype.delay = 1000 / NOMINAL_UPDATE_INTERVAL;
 
+//generate a random starting position and direction within the starting box 
+//for the ghosts when they spawn in the beginning of the game
 Ghost.prototype.randomisePosition = function () {
     // Rock randomisation defaults (if nothing otherwise specified)
     this.cx = this.cx || 360 + Math.random() * 80;
     this.cy = this.cy || 370 + Math.random() * 60;
-    var ran = Math.floor(Math.random()*4);
+    var ran = Math.floor(Math.random() * 4);
     switch (ran) {
         case 0:
-        this.velX = 1;
-        this.velY = 0;
+            this.velX = 1;
+            this.velY = 0;
             break;
         case 1:
-        this.velX = -1;
-        this.velY = 0;
+            this.velX = -1;
+            this.velY = 0;
             break;
         case 2:
-        this.velY = 1;
-        this.velX = 0;
+            this.velY = 1;
+            this.velX = 0;
             break;
         case 3:
-        this.velY = -1;
-        this.velX = 0;
+            this.velY = -1;
+            this.velX = 0;
             break;
 
         default:
@@ -96,20 +97,15 @@ Ghost.prototype.rememberResets = function () {
     this.reset_sprite = this.sprite;
 };
 
-
-
 // Initial, inheritable, default values
 Ghost.prototype.rotation = 0;
-//Ghost.prototype.velX = 0;
-//Ghost.prototype.velY = 1;
 
-
-
-Ghost.prototype.getSnake = function(){
-    var hitEntitys = spatialManager.findEntityInRange(this.cx,this.cy,10);
+//Killing the snake if the ghost and snake collide
+Ghost.prototype.getSnake = function () {
+    var hitEntitys = spatialManager.findEntityInRange(this.cx, this.cy, 10);
 
     hitEntitys.forEach(hitEntity => {
-        if(!hitEntity.isBlue && hitEntity.isHead){
+        if (!hitEntity.isBlue && hitEntity.isHead) {
             hitEntity.kill();
             console.log("killed by ghost");
             g_isUpdatePaused = true;
@@ -129,7 +125,7 @@ Ghost.prototype._moveToASafePlace = function () {
 
     for (var attempts = 0; attempts < 100; ++attempts) {
 
-        var warpDistance = 100 + Math.random() * g_canvas.width /2;
+        var warpDistance = 100 + Math.random() * g_canvas.width / 2;
         var warpDirn = Math.random() * consts.FULL_CIRCLE;
 
         this.cx = origX + warpDistance * Math.sin(warpDirn);
@@ -152,39 +148,41 @@ Ghost.prototype._moveToASafePlace = function () {
     }
 };
 
-Ghost.prototype.getBestMove = function() {
-    // AI pælingar random sammt ekki random eftir leveli ?
+//generating a move for the ghost trying to catch the snake a little
+Ghost.prototype.getBestMove = function () {
     var snakePos = entityManager.getSnakePos();
 
     var bestX = snakePos.cx - this.cx;
     var bestY = snakePos.cy - this.cy;
 
-    if (Math.abs(bestX) >= Math.abs(bestY)){
+    if (Math.abs(bestX) >= Math.abs(bestY)) {
         this.velY = 0;
-        this.velX = bestX/Math.abs(bestX);
-   } else {
+        this.velX = bestX / Math.abs(bestX);
+    } else {
         this.velX = 0;
-        this.velY = bestY/Math.abs(bestY);
+        this.velY = bestY / Math.abs(bestY);
     }
 }
 
-Ghost.prototype.getWorstMove = function() {
+//generating a move for the ghost trying to avoid the snake a little
+Ghost.prototype.getWorstMove = function () {
     var snakePos1 = entityManager.getSnakePos();
 
     var worstX = snakePos1.cx - this.cx;
     var worstY = snakePos1.cy - this.cy;
 
-    if (Math.abs(worstX) >= Math.abs(worstY)){
+    if (Math.abs(worstX) >= Math.abs(worstY)) {
         this.velX = 0;
-        this.velY = worstX/Math.abs(worstX);
-   } else {
+        this.velY = worstX / Math.abs(worstX);
+    } else {
         this.velY = 0;
-        this.velX = worstY/Math.abs(worstY);
+        this.velX = worstY / Math.abs(worstY);
     }
 }
 
-Ghost.prototype.getRandomMove = function() {
-    var rand = Math.floor(Math.random()*4 +1);
+//generating a random move for the ghost
+Ghost.prototype.getRandomMove = function () {
+    var rand = Math.floor(Math.random() * 4 + 1);
     switch (rand) {
         case 1:
             this.velX = 1;
@@ -214,7 +212,17 @@ Ghost.prototype.update = function (du) {
 
     spatialManager.unregister(this);
 
-    if(this._isDeadNow){
+    var speed = 1;
+    var num = 0;
+    var stig = game_score.get_score();
+
+    if(stig > num + 50){
+        speed *= 1.2;
+        num += 50;
+    }
+
+    //checking if the ghost is dead
+    if (this._isDeadNow) {
         return entityManager.KILL_ME_NOW;
     }
 
@@ -222,45 +230,65 @@ Ghost.prototype.update = function (du) {
 
     this.delay -= du;
 
-    if(entityManager.getSnakeIsBlue() && !this.hasRespawned){
+    //The ghosts become blue and edible if the snake is blue
+    if (entityManager.getSnakeIsBlue() && !this.hasRespawned) {
         this.isEdible = true;
         this.sprite = g_sprites.ghostEdible;
-    } else if (!entityManager.getSnakeIsBlue() && !this.hasRespawned){
+    } else if (!entityManager.getSnakeIsBlue() && !this.hasRespawned) {
         this.isEdible = false;
         this.sprite = this.reset_sprite;
     }
 
+    //the ghosts follow the snake with a little bit of tactic and 
+    //also run away not completly random
     this.getSnake();
-    if(this.delay < 0){
-        if(this.isEdible){
+    if (this.delay < 0) {
+        if (this.isEdible) {
             /// ATH ---- skoða eh skrítið
             //this.getWorstMove();
-            if (rand > 0.2){
+            if (rand > 0.2) {
                 this.getWorstMove();
-            }else {
+            } else {
                 this.getRandomMove();
             }
-        this.delay = 1000 / NOMINAL_UPDATE_INTERVAL;
-        }else{
-            if (rand > 0.2){
+            this.delay = 1000 / NOMINAL_UPDATE_INTERVAL;
+        } else {
+            if (rand > 0.2) {
                 this.getBestMove();
-            }else {
+            } else {
                 this.getRandomMove();
             }
-        this.delay = 1000 / NOMINAL_UPDATE_INTERVAL;
+            this.delay = 1000 / NOMINAL_UPDATE_INTERVAL;
         }
     }
     this.wrapPosition();
 
-    if(Level.checkCollisionGhost((this.cx + this.velX * du), (this.cy + this.velY * du))){
-        this.cx -= this.velX * du;
-        this.cy -= this.velY * du;
+    if (Level.checkCollisionGhost((this.cx + this.velX * du), (this.cy + this.velY * du))) {
+
+        //chooses a random number -1 or 1
+        var random = 1;
+        if (rand < 0.5) {
+            random = -1;
+        }
+
+        //if the ghosts hit a wall the turn left or right 
+        if (this.velX === 1) {
+            this.velX = 0;
+            this.velY = 1 * random;
+        } else if (this.velX === -1) {
+            this.velX = 0;
+            this.velY = 1 * random;
+        } else if (this.velY === 1) {
+            this.velX = 1 * random;
+            this.velY = 0;
+        } else if (this.velY === -1) {
+            this.velX = 1 * random;
+            this.velY = 0;
+        }
     } else {
-        this.cx += this.velX * du;
-        this.cy += this.velY * du;
+        this.cx += this.velX * speed * du;
+        this.cy += this.velY * speed * du;
     }
-
-
 
     spatialManager.register(this);
 
@@ -271,20 +299,23 @@ Ghost.prototype.getRadius = function () {
     return this.scale * (this.sprite.width / 2);
 };
 
+//if the ghost is eaten it dies and the player gets 20 points
 Ghost.prototype.eat = function () {
     game_score.add_score(20);
     this.kill();
 };
 
+//resetting the ghost 
 Ghost.prototype.reset = function () {
     this.setPos(this.reset_cx, this.reset_cy);
     this.rotation = this.reset_rotation;
     this.sprite = this.reset_sprite;
 };
 
-Ghost.prototype.respawn = function(){
+//the ghost then respawns in the middle 
+Ghost.prototype.respawn = function () {
     this.reset();
-    this.setPos(g_canvas.width*0.5,g_canvas.height*0.5);
+    this.setPos(g_canvas.width * 0.5, g_canvas.height * 0.5);
     this.isEdible = false;
     this.hasRespawned = true;
     this.resurrect();
@@ -293,6 +324,6 @@ Ghost.prototype.respawn = function(){
 
 Ghost.prototype.render = function (ctx) {
     this.sprite.drawWrappedCentredAt(
-	ctx, this.cx, this.cy, this.rotation
+        ctx, this.cx, this.cy, this.rotation
     );
 };
